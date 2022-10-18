92fe12307a9a04779e3862600d7b01cf3dc7f59a
afg-pradheebaa-srinivas <psrinivas@afg.tech>
2022-10-18 22:37:23 +0530

    todays data types

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace Lab1Project //school name
 {
-    internal class Program
+    internal class Program  //classname -- 1st class,2nd class
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // method names
         {
             Console.WriteLine("Hello World!");
+
+            int X = 100;
+            double = 5.99; 
+                decimal 
+            string str = "todays demo "
+
+                char singleletter ='A'
+
+                bool myconditioncheck = true;
+
+                
         }
     }
 }

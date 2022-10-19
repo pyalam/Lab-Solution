4ff99037625f4ac31dc01b7f93f2d490e9a77893
afg-pradheebaa-srinivas <psrinivas@afg.tech>
2022-10-19 23:02:50 +0530

    todays code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,16 +8,89 @@ namespace Lab1Project //school name
         {
             Console.WriteLine("Hello World!");
 
-            int X = 100;
-            double = 5.99; 
-                decimal
-            string str = "todays demo ";
+            // if else else if 
 
-            char singleletter = 'A';
+            //int i = 10;
 
-                bool myconditioncheck = true;
+            //int j = 20;
 
-                
+            //if (i < j)
+            //{
+            //    Console.WriteLine("i is less than j");
+            //}
+            //if(i > j)
+            //{
+            //    Console.WriteLine("i is greater than j");
+            //}
+
+            //int i = 20, j = 20;
+            //if (i > j)
+            //{
+            //    //condition1
+            //    Console.WriteLine(" i is greatr than j");
+            //}
+            //else if (i < j)
+            //{
+            //    Console.WriteLine(" i is less  than j");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" i is equal to j");
+            //}
+
+            // switch 
+
+            //int x = 100;
+
+            //switch (x)
+            //{
+            //    case 1:
+            //        Console.WriteLine(" value of x is 1");
+            //        break;
+            //        case 2:
+            //        Console.WriteLine(" value of x is 2");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("value of x is 3");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine(" value of x is 4");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("value of x is 10");
+            //        break;
+            //   default: Console.WriteLine(" value does not meet the switch condition");
+            //        break;
+            //}
+
+
+            // for loop //for each loop
+            // for (int i =0; i< 10; i++)
+            //{
+            //    Console.WriteLine("value of i {0}",i);
+            //}
+
+            //int x = 0;
+            //while (x < 10)
+            //{
+            //    Console.WriteLine("value of x is {0}", x);
+            //    x++;
+            //}
+
+            // while loop 
+            //do while loop
+
+            //foreachloop
+
+            //string[] cities = { "Dallas", "Chicago", "Milawaukee", "Houston" };
+            //foreach(var dummycity in cities)
+            //{
+            //    Console.WriteLine(dummycity);
+            //}
+
+
+
+
         }
     }
 }
